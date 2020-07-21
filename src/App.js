01d29a87371b237d36f3754a3300c55204777c4c
ernestognw <ernestognw@gmail.com>
2020-07-21 12:19:09 -0500

    feature(react): Add react setup
    React setup included basic initial hello world page, test setup, and public folder config files such as favicon and manifest.json

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <>
+      <img src="/images/general/placeholder.png" alt="Hello world" />
+      <p>Hello world</p>
+    </>
+  );
+};
+
+export default App;
