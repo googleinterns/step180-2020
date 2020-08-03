abc59f5bfdd8d82189a31a3eff3774c9807c4eea
ernestognw <ernestognw@gmail.com>
2020-08-03 00:32:28 -0500

    nitfix(general): Fix comments, and function namings
    Fix some grammatical errors, function namings and comment structure to follow @livvielin recommendations

--- src/config/theme/index.js
+++ src/config/theme/index.js
@@ -4,16 +4,16 @@ const theme = createMuiTheme({
   // Add special theming using this reference: https://material-ui.com/customization/default-theme/
   palette: {
     primary: {
+      contrastText: '#fff',
+      dark: '#006db3',
       light: '#63ccff',
       main: '#009be5',
-      dark: '#006db3',
-      contrastText: '#fff',
     },
     secondary: {
+      contrastText: '#fff',
+      dark: '#464c56',
       light: '#10161e',
       main: '#18202c',
-      dark: '#464c56',
-      contrastText: '#fff',
     },
   },
 });
