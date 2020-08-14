a4d787cb056a40ec6c9b7371f37bafb529870541
Jonathan Chavez <jonathanchatab@hotmail.com>
2020-08-14 18:54:36 -0500

    Fix JSDoc and add comments to describe some function behaviors

--- server/__tests__/api.test.js
+++ server/__tests__/api.test.js
@@ -3,6 +3,9 @@ import app from '../app';
 
 jest.mock('@google-cloud/bigquery');
 
+/**
+ * Test suite for the health check endpoint (/api)
+ */
 describe('Health Check', () => {
   it('Should be healthy', async () => {
     const res = await request(app).get('/api');
@@ -11,10 +14,16 @@ describe('Health Check', () => {
   });
 });
 
+/**
+ * Test suite for mixed-content endpoint (api/mixed-content)
+ */
 describe('mixed-content', () => {
   it('Check /top-websites-with-mixed-content', async () => {
-    const res = await request(app).get('/api/mixed-content/top-websites-with-mixed-content');
-    expect(res.body.description).toBe('Top websites with most mixed content and the percentage of mixed content in it.');
+    const res = await request(app).get('/api/mixed-content/'+
+    'top-websites-with-mixed-content');
+
+    expect(res.body.description).toBe('Top websites with most mixed content'+
+    'and the percentage of mixed content in it.');
     expect(res.body.result).toBeInstanceOf(Array);
     expect(res.body.suggestedVisualizations).toContain('Bar chart');
   });
