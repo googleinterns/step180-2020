111501a5c1499ec9884e7a8429da4b3ae7b2b280
Sofia Vega <sofiavega@google.com>
2020-08-14 15:07:06 -0600

    Remove tests

--- server/__tests__/api.test.js
+++ server/__tests__/api.test.js
@@ -1,21 +1,10 @@
 import request from 'supertest';
 import app from '../app';
 
-jest.mock('@google-cloud/bigquery');
-
 describe('Health Check', () => {
   it('Should be healthy', async () => {
     const res = await request(app).get('/api');
     expect(res.statusCode).toEqual(200);
     expect(res.text).toBe('API working');
-  });
-});
-
-describe('mixed-content', () => {
-  it('Check /top-countries-with-more-government-websites-with-mixed-content is working', async () => {
-    const res = await request(app).get('/api/mixed-content/top-countries-with-more-government-websites-with-mixed-content');
-    expect(res.body.description).toBe('Countries with more government websites that have mixed content');
-    expect(res.body.result).not.toBeNull();
-    expect(res.body.suggestedVisualizations).not.toBeNull();
   });
 });
